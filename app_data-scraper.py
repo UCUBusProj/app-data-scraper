075c8e409f287b9ba399862f53d464e62f8736a0
Maksym Gontar <max.gontar@gmail.com>
2017-07-16 22:24:25 +0300

    Clock

--- app_data-scraper.py
+++ app_data-scraper.py
@@ -1,12 +1,8 @@
-from flask import Flask
-
+from apscheduler.schedulers.blocking import BlockingScheduler
 import urllib.request
 import time
 import json
-
-app_finl = Flask(__name__)
-
-@app_finl.route('/')
+	
 def hello():
 	codes = ['LAD|712988','LAD|712991','LAD|713002','LAD|713010','LAD|1054553','LAD|949921','LAD|1723724','LAD|1527114']
 	code = 'LAD|712988'
@@ -18,5 +14,11 @@ def hello():
 	parsed_json = json.loads(data[codes[0]].decode('utf-8').replace('"[', '[').replace(']"', ']').replace('\\"', '"'))
 	return str(parsed_json[7])
 
-if __name__ == '__main__':
-    app_finl.run()
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=2)
+def timed_job():
+    print(hello())
+
+sched.start()
+
