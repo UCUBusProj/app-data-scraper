4f9e6889d4843fe2a102fa121cc9fda25fc32e50
Maksym Gontar <max.gontar@gmail.com>
2017-07-16 22:38:46 +0300

    update task

--- app_data-scraper.py
+++ app_data-scraper.py
@@ -5,14 +5,13 @@ import json
 	
 def hello():
 	codes = ['LAD|712988','LAD|712991','LAD|713002','LAD|713010','LAD|1054553','LAD|949921','LAD|1723724','LAD|1527114']
-	code = 'LAD|712988'
+	#code = 'LAD|712988'
 	data = {}
-	#for code in codes:
-		#time.sleep(5) 
-	with urllib.request.urlopen('http://82.207.107.126:13541/SimpleRide/LAD/SM.WebApi/api/RouteMonitoring/?code='+code) as response:
-		data[code] = response.read()
-	parsed_json = json.loads(data[codes[0]].decode('utf-8').replace('"[', '[').replace(']"', ']').replace('\\"', '"'))
-	return str(parsed_json[7])
+	for code in codes:
+		time.sleep(5) 
+		with urllib.request.urlopen('http://82.207.107.126:13541/SimpleRide/LAD/SM.WebApi/api/RouteMonitoring/?code='+code) as response:
+			data[code] = json.loads(response.read().decode('utf-8').replace('"[', '[').replace(']"', ']').replace('\\"', '"'))
+	return str(data)
 
 sched = BlockingScheduler()
 
